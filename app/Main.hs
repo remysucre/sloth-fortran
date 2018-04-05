a6933d01fab9e3eb34ccbf5ce0f59437f0f22313
Remy Wang <remywang@uw.edu>
2018-04-05 12:56:44 -0700

    loops

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,24 @@
 module Main where
 
 import Lib
+import Language.Fortran.PrettyPrint
+import Language.Fortran.AST
+import Language.Fortran.ParserMonad
+import Language.Fortran.Parser.Fortran90
+import qualified Data.ByteString as BS
+import System.Environment
+import Debug.Trace
 
 main :: IO ()
-main = someFunc
+main = do
+  [fn] <- getArgs
+  fc <- BS.readFile fn
+  -- TODO update to call the right parser
+  let fortran = case fortran90Parser fc fn
+                  of ParseOk pt _ -> pt
+                     _ -> trace fn $ ProgramFile (MetaInfo Fortran90 "") []
+      res = testj fortran
+      prettyPrint = \r -> pprintAndRender Fortran90 r fixedForm
+      ms = concatMap ((++ "haha \n") . prettyPrint) $ res
+      out = fn ++ "\n" ++ ms ++ "matches" ++ (show $ length res) ++ "\n"
+  if length res > 0 then putStr out else return ()
--- src/Lib.hs
+++ src/Lib.hs
@@ -1,6 +1,7 @@
-module Lib
-    ( someFunc
-    ) where
+module Lib where
 
-someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+import Language.Fortran.AST
+import Data.Generics.Uniplate.Data
+
+testj :: ProgramFile A0 -> [Block A0]
+testj p = [r | r@(BlDo _ _ _ _ _ _ _ _) <- universeBi p]
